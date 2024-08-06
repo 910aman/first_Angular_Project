056559c036a2bb2f6e936abf8e5a831c9df89c6b
910aman <amanchauhan9426@gmail.com>
2024-08-06 18:07:01 +0530

    error while Posting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'first_Angular_Project';
+  title = 'Angular Project';
 }
